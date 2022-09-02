de4c6e9011ad1eb3812c10cf91f41330de6836d0
Patrick Geoghegan <patrick.geoghegan@volograms.com>
2022-09-02 11:54:27 +0200

    Added app

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/voljs/',
+  plugins: [react(), mkcert()],
+  server: {
+    host: true,
+    https: true,
+  },
+  preview: {
+    host: true,
+    https: true,
+  }
+})
